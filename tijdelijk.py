8040926996e37a17962e840d3660c82754cf3036
R. Uitterlinden <utl_webinfo@planet.nl>
2023-03-02 16:17:50 +0100

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,16 +1,21 @@
-# Les 7
-dictionary_prijzen = {
-    "aardbei": 3,
-    "vanille" : 4,
-    "chocolade" : 5
-    }
-aanbieding = dictionary_prijzen["vanille"] *0.8
-reclame_tekst=f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
-reclame_tekst3 = reclame_tekst.upper()
-reclame_tekst4 = reclame_tekst3.split(" ")
-# opdracht 10
-for el in reclame_tekst4:
-    if len(el) >= 5:
-        print (el.upper())
-    else:
-        print (el.lower())
+from helper import decoreer
+
+def print_aanbieding():
+    dictionary_prijzen = {
+        "aardbei": 3,
+        "vanille" : 4,
+        "chocolade" : 5
+        }
+    aanbieding = dictionary_prijzen["aardbei"] *0.8
+    reclame_tekst=f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+    reclame_tekst2 =reclame_tekst[:63]
+    reclame_tekst3 = reclame_tekst2.upper()
+    reclame_tekst4 = reclame_tekst3.split(" ")
+    # opdracht 10
+    for el in reclame_tekst4:
+        if len(el) >= 5:
+            print (el.upper())
+        else:
+            print (el.lower())
+decoreer("Aanbieding")
+print_aanbieding()
